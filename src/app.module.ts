893246ff0a60ca7fd5b0d5c916d264727881df47
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2022-11-03 05:12:14 +0600

    changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoStore } from './todo/todo.store';
 import { TodoController } from "./todo/todo.controller";
-import { join } from "path";
-import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-    imports: [
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', 'static'),
-        }),
-    ],
     controllers: [ TodoController ],
     providers: [ TodoStore ]
 })
-export class AppModule {
-}
+
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
@@ -9,10 +8,6 @@ async function bootstrap() {
     );
 
     app.enableCors({ origin: 'localhost' });
-
-    app.useStaticAssets(join(__dirname, '..', 'public'));
-    app.setBaseViewsDir(join(__dirname, '..', 'views'));
-    app.setViewEngine('hbs');
 
     await app.listen(3000);
 }
